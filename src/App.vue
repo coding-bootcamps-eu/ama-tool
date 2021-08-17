b89b47d0a125cd7650e71836a8125e7665c22074
BringsCoding <simonbrings@googlemail.com>
2021-08-17 16:47:46 +0200

    feat: start to inplement footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <MainNavigation />
+  <MainFooter />
   <router-view />
 </template>
 
 <script>
 import MainNavigation from "@/components/MainNavigation/MainNavigation.vue";
+import MainFooter from "@/components/MainFooter/MainFooter.vue";
 
 export default {
   name: "App",
-  components: {MainNavigation},
-}
+  components: {
+    MainNavigation,
+    MainFooter,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -17,13 +22,13 @@ export default {
   box-sizing: border-box;
 }
 #app {
-  --primary-color: #6A1CC3;
-  --secondary-color: #A970EB;
+  --primary-color: #6a1cc3;
+  --secondary-color: #a970eb;
   --background-color: #fafafa;
-  --success-color: #A3DB33;
-  --fail-color: #ED3221;
+  --success-color: #a3db33;
+  --fail-color: #ed3221;
   --font-color: #121212;
-  --placeholder-color: rgba(18, 18, 18, .7);
+  --placeholder-color: rgba(18, 18, 18, 0.7);
 
   @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
   font-family: "Open Sans", sans-serif;
@@ -36,12 +41,17 @@ html {
   color: var(--font-color);
   font-size: 18px;
   line-height: 1.5rem;
-  h1{font-size: 2.2rem};
-  h2{font-size: 2.0rem};
-  h3{font-size: 1.8rem};
+  h1 {
+    font-size: 2.2rem;
+  }
+  h2 {
+    font-size: 2rem;
+  }
+  h3 {
+    font-size: 1.8rem;
+  }
 }
 body {
   margin: 0;
 }
-
 </style>
---
+++ src/components/MainFooter/MainFooter.vue
@@ -0,0 +1,80 @@
+<template>
+  <footer>
+    <div class="footer-wrapper">
+      <ul>
+        <li>
+          <a
+            href="https://www.coding-bootcamps.eu/j/privacy"
+            target="_blank"
+            rel="noopener"
+            >Datenschutz</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.coding-bootcamps.eu/about/"
+            target="_blank"
+            rel="noopener"
+            >Impressum</a
+          >
+        </li>
+        <li>
+          <router-link to="/team">Team</router-link>
+        </li>
+        <li>
+          <a
+            href="https://www.coding-bootcamps.eu/bewerben/"
+            target="_blank"
+            rel="noopener"
+            >Bewerben</a
+          >
+        </li>
+      </ul>
+      <p>Copyright 2021</p>
+      <div class="footer-social-wrapper">
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/codingbootcamps.eu/"
+              target="_blank"
+              rel="noopener"
+              >Instagram</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/CodeCampEurope"
+              target="_blank"
+              rel="noopener"
+              >Twitter</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.image-footer {
+  height: 1.5rem;
+}
+a {
+  text-decoration: none;
+}
+li {
+  list-style-type: none;
+  text-align: left;
+}
+.footer-wrapper {
+  display: flex;
+  justify-content: start;
+  align-items: flex-end;
+}
+ul {
+  padding-right: 4rem;
+}
+.footer-social-wrapper {
+  padding: 0 4rem;
+}
+</style>
