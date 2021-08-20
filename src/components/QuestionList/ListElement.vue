5774f8988fdb502e056cb429a74699920544a757
ChristianMLux <christian.m.lux@gmail.com>
2021-08-20 10:07:29 +0200

    feat: styles the listelements

--- src/components/QuestionList/ListElement.vue
+++ src/components/QuestionList/ListElement.vue
@@ -3,22 +3,33 @@
     <div class="question-heading">
       <h3>{{ questionTitle }}</h3>
       <div class="inner-question-wrapper">
+        <p>Frage von: {{ author }}</p>
         <p>Kategorie: {{ questionCat }}</p>
+        <p>Erstellt am: {{ created_at }}</p>
       </div>
     </div>
     <div class="question-edit">
-      <button :disabled="isDone" @click="$emit('answer')">
+      <button class="answer-button" :disabled="isDone" @click="$emit('answer')">
         <p v-if="isDone">Erledigt</p>
         <p v-else>Beantworten</p>
       </button>
     </div>
     <div class="vote-wrapper">
       <p>Votes: {{ upvotes }}</p>
-      <button :disabled="userVoted" @click="$emit('upvote')">
-        <span v-if="!userVoted">vote</span>
+      <div class="vote-arrows-wrapper"></div>
+      <button
+        class="vote-button-up"
+        :disabled="userVoted"
+        @click="$emit('upvote')"
+      >
+        <i class="fi-rr-angle-up" v-if="!userVoted"></i>
       </button>
-      <button :disabled="!userVoted" @click="$emit('downvote')">
-        <span v-if="userVoted">Downvote</span>
+      <button
+        class="vote-button-down"
+        :disabled="!userVoted"
+        @click="$emit('downvote')"
+      >
+        <i class="fi-rr-angle-down" v-if="userVoted"></i>
       </button>
     </div>
   </li>
@@ -77,12 +88,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url("https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css");
 li {
   margin: 1rem 4rem 1rem 0;
   list-style-type: none;
   justify-content: space-between;
-  border: 0.5px solid var(--primary-color);
+  align-items: center;
+  box-shadow: rgba(106, 28, 195, 1) 0px 0.5px 1px 0px,
+    rgba(169, 112, 235, 1) 0px 1px 3px 1px;
+  _border: 0.5px solid var(--primary-color);
   border-radius: 0.25rem;
+  .question-heading {
+    margin: 0.5rem 2rem 0.5rem 1rem;
+    min-width: 35%;
+    max-width: 70%;
+  }
+  .inner-question-wrapper {
+    display: flex;
+    flex-flow: row;
+    align-items: baseline;
+    p {
+      font-size: 0.9em;
+      margin-right: 1rem;
+    }
+  }
   div {
     margin-left: 1rem;
     display: flex;
@@ -102,6 +131,45 @@ li {
   }
   button {
     margin: 1rem;
+  }
+  .answer-button {
+    padding: 0 0.5rem;
+    background-color: var(--primary-color);
+    color: var(--background-color);
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    p {
+      margin: 0;
+      font-weight: bold;
+    }
+  }
+  .vote-wrapper {
+    margin: 0.5rem 2rem;
+    text-align: center;
+  }
+  .vote-button-up {
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    i {
+      font-size: 1.7rem;
+      color: var(--success-color) !important;
+      font-weight: bold !important;
+    }
+  }
+  .vote-button-down {
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    i {
+      font-size: 1.7rem;
+      color: var(--fail-color) !important;
+      font-weight: bold !important;
+    }
   }
 }
 </style>
