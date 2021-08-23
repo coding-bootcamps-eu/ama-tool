4b12bf636ee15cc68cb5dcf3bb3ed9582f8d7433
atef <mail@aamri.de>
2021-08-23 17:00:43 +0200

    feat: implements db testcomponents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home">
+    <AskQuestion />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import AskQuestion from "@/components/QuestionList/AskQuestion.vue";
 
 export default {
   name: "Home",
+  components: {
+    AskQuestion,
+  },
+
 };
+
 </script>
