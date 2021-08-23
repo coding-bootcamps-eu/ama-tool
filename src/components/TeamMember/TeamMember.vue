58680ccfd01fc247fd5737616944525fca0373bf
Christian Glissmann <cgliss2000@gmail.com>
2021-08-23 12:47:56 +0200

    initial commit

---
+++ src/components/TeamMember/TeamMember.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="member-picture" src="@/assets/deniz.jpeg" />
+</template>
+
+<script>
+export default {
+  name: "TeamMember",
+};
+</script>
+
+<style lang="scss" scoped>
+.member-picture {
+  border-radius: 50%;
+  border: 4px solid var(--primary-color);
+  transform: scale(0.5);
+}
+</style>
---
+++ src/views/Team.vue
@@ -0,0 +1,12 @@
+<template>
+  <TeamMember />
+</template>
+
+<script>
+import { TeamMember } from "@/components/TeamMember/TeamMember.vue";
+
+export default {
+  name: "Team",
+  components: { TeamMember },
+};
+</script>
