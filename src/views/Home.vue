b6a3ea0454033d34a8a78df70fcbb89ea830538a
atef <mail@aamri.de>
2021-08-24 23:13:06 +0200

    feature: implements Firebase Authentication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <button @click="githubSignin">Github Signin</button>
+    <button @click="githubSignout">Github Signout</button>
+  </div>
 </template>
 
 <script>
@@ -7,5 +10,45 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      snackbar: false,
+      snackbarText: "No error message",
+    };
+  },
+  // methods: {
+  //   githubSignin() {
+  //     // With popup.
+  //     var provider = new firebase.auth.GithubAuthProvider();
+  //     provider.addScope("repo");
+  //     firebase
+  //       .auth()
+  //       .signInWithPopup(provider)
+  //       .then(function (result) {
+  //         // This gives you a GitHub Access Token.
+  //         var token = result.credential.accessToken;
+  //         // The signed-in user info.
+  //         var user = result.user;
+  //       })
+  //       .catch(function (error) {
+  //         // Handle Errors here.
+  //         var errorCode = error.code;
+  //         var errorMessage = error.message;
+  //         // The email of the user's account used.
+  //         var email = error.email;
+  //         // The firebase.auth.AuthCredential type that was used.
+  //         var credential = error.credential;
+  //         if (errorCode === "auth/account-exists-with-different-credential") {
+  //           alert(
+  //             "You have signed up with a different provider for that email."
+  //           );
+  //           // Handle linking here if your app allows it.
+  //         } else {
+  //           console.error(error);
+  //         }
+  //       });
+  //   },
+  //   githubSignout() {},
+  // },
 };
 </script>
