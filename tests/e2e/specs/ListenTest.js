1de77e1d6e855d9f55f3d4c07c134c3974e42f2d
BringsCoding <simonbrings@googlemail.com>
2021-08-26 11:17:52 +0200

    fix:data-cy

--- tests/e2e/specs/ListenTest.js
+++ tests/e2e/specs/ListenTest.js
@@ -1,13 +1,13 @@
-describe("Test Fragen liste exist", () => {
-  it("Fragen liste should exist", () => {
+describe("Testing the range Fragen List", () => {
+  it("exists the area Fragen List", () => {
     cy.visit("/questionlist");
     cy.url().should("exist").should("include", "/questionlis");
   });
 
-  it("Test Check the focus", () => {
-    cy.get("[data-cy='input-radio']").eq(0).focus();
-    cy.get("[data-cy='input-radio']").eq(1).focus();
-    cy.get("[data-cy='input-radio']").eq(2).focus();
+  it("Testing the focus by the radio button and answer button", () => {
+    cy.get("[data-cy='input-radio-alle-fragen']").focus();
+    cy.get("[data-cy='input-radio-offene-fragen']").focus();
+    cy.get("[data-cy='input-radio-beantwortete-fragen']").focus();
 
     cy.get("[data-cy='answer-btw']").eq(0).focus();
     cy.get("[data-cy='answer-btw']").eq(1).focus();
@@ -18,13 +18,13 @@ describe("Test Fragen liste exist", () => {
     /* we wait of the firebase */
   });
 
-  it("Test Check the Filter and Buttons", () => {
+  it("Testing the Filter and Buttons", () => {
     cy.get("[data-cy='answer-btw']").eq(1).contains("Beantworten").click();
     cy.get("[data-cy='withdraw-btw']").contains("Zurückziehen");
     cy.get("[data-cy='single-filter-wrapper']").click({ multiple: true });
   });
 
-  it("Test Check the up and downvotes", () => {
+  it("Testing Check the up and downvotes", () => {
     cy.get("[data-cy='single-filter-wrapper']").eq(0).click();
     cy.get("[data-cy='vote-button-up']").eq(0).click();
   });
