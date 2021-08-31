9e2adc9fba47f14b4d659bd1a2679d173b2d4614
Deniz Dalsar <deniz.d3811@outlook.de>
2021-08-31 10:22:56 +0200

    feat:Landing Page

---
+++ src/components/LandingPage/LandingPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <h2>Das Tool</h2>
+
+  <p>
+    Im Rahmen unseres Abschlussprojekts haben wir dieses Tool erstellt, um das
+    Leben im Bootcamp einfacher zu gestalten und unsere Fähigkeiten zu
+    erweitern. <br />
+    Dieses Tool dient der Vorbereitung auf das Ask me anything und soll eine
+    einfache Übersicht über Fragen und Themen bieten.
+  </p>
+  <p>
+    Im Navigationspunkt „Frageliste“ seht ihr alle Fragen, die bisher gestellt
+    wurden. Wenn eure Frage in dieser Übersicht bereits auftaucht, müsst ihr sie
+    nicht erneut stellen. Stattdessen könnt ihr sie upvoten. Je mehr Upvotes,
+    desto schneller wird die Frage beantwortet. <br />
+    Ihr könnt die Fragen auch filtern, um nur beantwortete oder unbeantwortete
+    Fragen zu sehen. Allerdings können nur Teachers Fragen als „beantwortet“
+    markieren.
+  </p>
+  <p>
+    Unter dem Punkt „Frage stellen“ könnt ihr eure Fragen, die noch nicht in der
+    Liste auftauchen, einreichen. Gebt der Frage einen aussagekräftigen Titel
+    und beschreibt sie im Feld darunter genauer. Dort könnt ihr Markdown
+    benutzen und auch eine Vorschau davon einblenden.<br />
+    Wählt eine Kategorie, die am besten zu eurer Frage passt, um sie einfacher
+    einordnen zu können. Nach dem Senden erscheint die Frage in der Frageliste.
+  </p>
+  <p>
+    Auf der Team-Seite wird euch das Team vorgestellt, das diese Seite erstellt
+    hat. Vielleicht findet ihr ja auch ein kleines Easteregg?
+  </p>
+</template>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+p {
+  margin: auto;
+  margin-top: 1rem;
+  font-family: "Open Sans", sans-serif;
+  font-size: 18px;
+  text-align: left;
+  width: 60vw;
+  min-width: 27rem;
+  max-width: 40rem;
+  line-height: 1.7rem;
+}
+h2 {
+  font-size: 2rem;
+}
+</style>
