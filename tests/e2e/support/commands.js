45bb107a676cc18d73df73d02c740a17fddc1277
CML <christian.m.lux@gmail.com>
2021-09-08 12:31:45 +0200

    fix: implements cypress login function

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  window.sessionStorage.setItem("userID", "1");
+  cy.visit("http://localhost:8080");
+});
