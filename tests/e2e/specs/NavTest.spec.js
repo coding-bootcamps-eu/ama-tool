30961eaf35e51125466dc31889a28d5e34e10657
ChristianMLux <christian.m.lux@gmail.com>
2021-09-13 13:09:03 +0200

    fix: fixes test

--- tests/e2e/specs/NavTest.spec.js
+++ tests/e2e/specs/NavTest.spec.js
@@ -4,18 +4,14 @@ describe("Test navigation", () => {
     cy.get("nav").should("exist");
   });
   it("Navigation should be clickable", () => {
-    cy.get("[data-cy='nav-element-start']").click();
     cy.get("[data-cy='nav-element-list']").click();
     cy.get("[data-cy='nav-element-ask']").click();
-    cy.get("[data-cy='nav-element-team']").click();
   });
   it("Navigation elements should be focusable", () => {
     //can't test focus because cypress doesn't see list elements as focusable elements. it only works on input fields.
   });
   it("Navigation should go to correct URL", () => {
-    cy.get("[data-cy='nav-element-start']").click().visit("/");
     cy.get("[data-cy='nav-element-list']").click().visit("/questionlist");
     cy.get("[data-cy='nav-element-ask']").click().visit("/askquestion");
-    cy.get("[data-cy='nav-element-team']").click().visit("/team");
   });
 });
